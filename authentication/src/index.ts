3c9ae572708111af8e00f5203a913dea300abcfd
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2023-01-31 10:37:34 -0400

    add project

---
+++ authentication/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hello Bader!!')
+});
+
+app.listen(3000, () => {
+    console.log("Listening at port: 3000!!!")
+});
