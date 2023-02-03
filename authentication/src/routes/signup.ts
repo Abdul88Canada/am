9c8fea60c2baa42d03eaa5679d97cb800f1b2068
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2023-02-03 11:09:41 -0400

    started impelmeting signin flow

--- authentication/src/routes/signup.ts
+++ authentication/src/routes/signup.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt  from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-valdiation-error';
-import { DBConnectionError } from '../errors/db-connection-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -18,13 +17,8 @@ router.post('/api/users/signup', [
         .isLength({ min: 4, max: 20})
             .withMessage('Password must be between 4 and 20 characters')
     ], 
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array()); 
-        }
-        
         const { email, userName, phoneNumber, password } = req.body;
 
         const existingEmail = await User.findOne({ email });
