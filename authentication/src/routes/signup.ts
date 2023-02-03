8fb3c293bcbad1a75630dbbe44156202873ac831
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2023-02-02 22:18:52 -0400

    Emplemented jwt for signup. Changed the returned object from db to be json without password

--- authentication/src/routes/signup.ts
+++ authentication/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt  from 'jsonwebtoken';
 
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-valdiation-error';
@@ -44,6 +45,17 @@ router.post('/api/users/signup', [
 
         const user = User.build({email, userName, phoneNumber, password});
         await user.save();
+
+        // generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
+
+        // store it on session object
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(user);
 });
