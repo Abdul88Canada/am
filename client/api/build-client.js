713b1a1e0c6ff713df8a1c44f56b5c11a2d56ff5
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2023-02-05 20:56:32 -0400

    completed the check from landing page to see if the user is signed in or not

---
+++ client/api/build-client.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default ({ req }) => {
+    if (typeof window === 'undefined') {
+        // we are on a server
+        return axios.create({
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            headers: req.headers
+        })
+    } else {
+        // we are on a broswer
+        return axios.create({
+            baseURL: '/'
+        })
+    }
+}
