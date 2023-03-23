1263de18940ba26bbdd5a5405aa09575849ae8c5
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2023-03-23 18:07:36 -0300

    Updates

--- common/src/events/user-updated-event.ts
+++ common/src/events/user-updated-event.ts
@@ -4,11 +4,9 @@ export interface UserUpdatedEvent {
     subject: Subjects.UserUpdated;
     data: {
         user_id: String,
-        full_name: string,
         user_type: string,
         created_at: Date,
-        owner_id: String,
         userName: String,
-        linked_properties: [String]
+        campaigns: [String]
     }
 }
